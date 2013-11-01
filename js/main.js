969618706cecbb78ac4578ab232e5abb681734a4
Michael Degtyarev <mdegtyarev@Michaels-Mac-Pro.local>
2013-11-01 14:54:15 -0600

    Added initial Angular code to populate table of auctions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+var app = angular.module('myApp', []);
 
+app.controller('AppCtrl', function($scope, $log) {
+	$log.log("initializing AppCtrl");
+	$scope.items = [
+		{ expiresIn: '15 min', name: 'name', highestBid: '25.00', highestBidder: 'Rudy', myBid: '10.00' },
+		{ expiresIn: '16 min', name: 'name', highestBid: '20.00', highestBidder: 'Rudy', myBid: '10.00' },
+		{ expiresIn: '17 min', name: 'name', highestBid: '28.00', highestBidder: 'Rudy', myBid: '10.00' },
+		{ expiresIn: '18 min', name: 'name', highestBid: '25.25', highestBidder: 'Rudy', myBid: '10.00' }
+	];
+});
+	
